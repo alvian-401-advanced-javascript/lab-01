c1af4ae653cb7b7ea754dbedc03920059d72d52a
Alvian Joseph <alvianjoseph@gmail.com>
2019-06-25 20:33:33 -0700

    adds faker, all tests passing

--- __tests__/greet.test.js
+++ __tests__/greet.test.js
@@ -1,14 +1,20 @@
 'use strict';
+const faker = require('faker');
 
 const greet = require('../lib/greet.js');
 
 describe('say hello function', () => {
     it('returns concatenation of hello + a string param', () => {
         let testString = 'AJ';
-        expect(greet.sayHello(testString)).toStrictEqual('Hello, AJ');
+        let randomName = faker.name.findName().toString();
+        expect(greet.sayHello(randomName)).toStrictEqual(`Hello, ${randomName}`);
     });
     it('returns null if param is not a string', () => {
         let notAstring = 12345;
         expect(greet.sayHello(notAstring)).toBeNull();
     });
+    it('returns hello world', () => {
+        let input = 'world';
+        expect(greet.sayHello(input)).toStrictEqual('Hello, world');
+    } )
 });
